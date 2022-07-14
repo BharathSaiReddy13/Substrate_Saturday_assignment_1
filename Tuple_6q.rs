092a1518b9c914ebe3c0420868fdcef6bbf4fac5
BharathSaiReddy13 <109013510+BharathSaiReddy13@users.noreply.github.com>
2022-07-14 21:33:13 +0530

    Create Tuple_6q.rs

---
+++ Tuple_6q.rs
@@ -0,0 +1,15 @@
+//6q
+//tuples can be passed as function arguments and return type
+fn main() {
+    // Fill the blank, need a few computations here.
+    let (x, y) = sum_multiply((2, 3));
+
+    assert_eq!(x, 5);
+    assert_eq!(y, 6);
+
+    println!("Success!");
+}
+
+fn sum_multiply(nums: (i32, i32)) -> (i32, i32) {
+    (nums.0 + nums.1, nums.0 * nums.1)
+}
