827a7ed6066f4ed0f35c110cac4e0f6a48ee1cb0
BharathSaiReddy13 <109013510+BharathSaiReddy13@users.noreply.github.com>
2022-07-14 21:11:25 +0530

    Create slice_1q.rs

---
+++ slice_1q.rs
@@ -0,0 +1,10 @@
+//1q
+slice should be used with references
+fn main() {
+    let arr = [1, 2, 3];
+    let s1: &[i32] = &arr[0..2];
+
+    let s2: &str = "hello, world" as &str;
+
+    println!("Success!");
+}
