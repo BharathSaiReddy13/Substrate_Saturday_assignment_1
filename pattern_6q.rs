02eaa81596be6eb64111482d8b984becaf60b434
BharathSaiReddy13 <109013510+BharathSaiReddy13@users.noreply.github.com>
2022-07-14 23:03:08 +0530

    Create pattern_6q.rs

---
+++ pattern_6q.rs
@@ -0,0 +1,10 @@
+//6q
+fn main() {
+    let mut v = String::from("hello,");
+    let r = &mut v;
+
+    match r {
+        // The type of value is &mut String
+       value => value.push_str(" world!") 
+    }
+}
