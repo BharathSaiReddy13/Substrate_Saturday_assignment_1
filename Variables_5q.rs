bc3577d66143b6f0cc3083315ef8ecbf711ae61c
BharathSaiReddy13 <109013510+BharathSaiReddy13@users.noreply.github.com>
2022-07-14 23:11:41 +0530

    Update Variables_5q.rs

--- Variables_5q.rs
+++ Variables_5q.rs
@@ -12,3 +12,15 @@
 //    let x =  42;
 //    println!("{}", x); // Prints "42".
 //}
+fn main() {
+    let x: i32 = 5;
+    {
+        let x = 12;
+        assert_eq!(x, 12);
+    }
+
+    assert_eq!(x, 5);
+
+    let x = 42;
+    println!("{}", x); // Prints "42".
+}
