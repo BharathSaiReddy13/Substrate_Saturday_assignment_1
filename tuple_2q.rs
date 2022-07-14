0c18ec1ee488b443e79271b7d5a10d5b3697ed64
BharathSaiReddy13 <109013510+BharathSaiReddy13@users.noreply.github.com>
2022-07-14 21:28:12 +0530

    Create tuple_2q.rs

---
+++ tuple_2q.rs
@@ -0,0 +1,8 @@
+//2q
+// members of tuple are extracted using tuple indexing t.0 return element in 0th index of tuple
+fn main() {
+    let t = ("i", "am", "sunface");
+    assert_eq!(t.2, "sunface");
+
+    println!("Success!");
+}
