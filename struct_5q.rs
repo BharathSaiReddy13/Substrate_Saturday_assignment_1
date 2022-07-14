515270e65d8e1f64cf533f4c5a8df666e817dfca
BharathSaiReddy13 <109013510+BharathSaiReddy13@users.noreply.github.com>
2022-07-14 22:02:32 +0530

    Create struct_5q.rs

---
+++ struct_5q.rs
@@ -0,0 +1,16 @@
+//5q
+struct Person {
+    name: String,
+    age: u8,
+}
+fn main() {
+    println!("Success!");
+} 
+
+fn build_person(name: String, age: u8) -> Person {
+    Person {
+        name,
+        age,
+        
+    }
+}
